5580ac2dfd06f03331d7632936720f1d6413e818
Kristian Jabilles <kristian.theodore@moneysmart.co>
2018-08-28 20:51:15 +0800

    Step 4: Layouts

---
+++ layouts/search.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="main">
+    <input type="text" placholder="Search">
+    <div class="content">
+      <nuxt/>
+    </div>
+  </div>
+</template>
---
+++ pages/search.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <div>
+      Search Results
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'search'
+}
+</script>
