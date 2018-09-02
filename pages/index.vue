45d68826dfcff9857550422c7812c92017589c93
Kristian Jabilles <kristian.theodore@moneysmart.co>
2018-09-02 11:32:15 +0800

    Step 7: Computed and Watcher Properties

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,28 @@
         <input type="text" v-model="firstName" placeholder="Enter firstname">
         <p>First Name: {{firstName}}</p>
       </section>
-      <br/>
       <section>
         <button @click="counter += 1">Add 1</button>
         <p>The button above has been clicked {{ counter }} times.</p>
       </section>
-      <br />
+      <section>
+        <p>{{ count }}</p>
+      </section>
+      <section>
+        <p>{{ reversedFirstName }}</p>
+      </section>
       <section>
         <button @click="say('wazzup')">Say hi</button>
       </section>
-      <br />
       <section>
         <button @click="isVisible = !isVisible">toggle</button>
         <h1 v-show="isVisible">Hello!</h1>
       </section>
-      <br />
       <section>
         <p v-if="counter > 10">{{ counter }} in stock.</p>    
         <p v-else-if="counter > 0">Hurry up, there are just a few items left!</p>    
         <p v-else>Oops! Sorry, we're all out!</p>  
       </section>
-      <br />
       <section>
         <h4>TODOS</h4>
         <ul class="todos">
@@ -34,7 +35,6 @@
           </li>
         </ul>
       </section>
-      <br />
       <section>
         <p v-html="rawHTML" />
       </section>
@@ -54,12 +54,31 @@ export default {
         {title: 'Meeting with Kelly'},
         {title: '1-on-1 with Vince'}
       ],
-      rawHTML: '<h1>Luke!</h1> <span>I am your father.</span> '
+      rawHTML: '<h1>Luke!</h1> <span>I am your father.</span> ',
+      products: [1, 2, 3]
     }
   },
   methods: {
     say(message) {
       alert(message);
+    },
+    updateInfo(){
+      alert('info updated!')
+    }
+  },
+  computed: {
+    count() {
+      return 'The count is ' + this.products.length * 10
+    },
+    reversedFirstName() {
+      return this.firstName.split('').reverse().join('')
+    }
+  },
+  watch: {
+    counter(newVal, oldVal) {
+      if(newVal === 5) {
+        this.updateInfo()
+      }
     }
   }
 }
